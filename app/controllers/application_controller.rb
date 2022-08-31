ef8a7d195443e56cca6dec89c70ad59a4d0b6ac4
TabithaKoech <tabbyk1718@gmail.com>
2022-08-31 15:02:46 +0300

    commit tree 3fcc8e4d6cc70a8f054bf18e0252ea0f0dd0f9e9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,30 @@
 class ApplicationController < Sinatra::Base
+    # Add this line to set the Content-Type header for all responses
+    set :default_content_type, 'application/json'
 
-  get '/' do
-    { message: "Hello world" }.to_json
+
+
+  # get '/' do
+  #   { message: "Hello world" }.to_json
+  # end
+  get '/games' do
+    # get all the games from the database
+    # games = Game.all
+    games = Game.all.order(:title).limit(10)
+
+    # return a JSON response with an array of all the game data
+    games.to_json
   end
+  get '/games/:id' do
+    # get the game with the id from the params
+    game = Game.find(params[:id])
+
+    # return a JSON response with the game data
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })    end
+
 
 end
